6878d2eba29f55b561f06115057351d063df2b4a
Devashree Lonkar <devashrilonkar2016@gmail.com>
2025-07-13 23:44:28 +0530

    Multithreading Example

---
+++ src/Multithreading/SleepMethod.java
@@ -0,0 +1,23 @@
+package Multithreading;
+
+public class SleepMethod {
+
+	public static void main(String[] args) {
+		 SleepExample t1 = new SleepExample();
+	        t1.start();
+	    }
+	}
+
+	class SleepExample extends Thread {
+	    public void run() {
+	        for (int i = 1; i <= 5; i++) {
+	            System.out.println(Thread.currentThread().getName() + " - Count: " + i);
+	            try {
+	                Thread.sleep(2000); // Sleep for 2 second
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	}
+
