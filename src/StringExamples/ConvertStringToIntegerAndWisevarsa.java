cf4424181ee49e74545ee72cc003ad061b5a4950
Devashree Lonkar <devashrilonkar2016@gmail.com>
2025-07-30 23:01:32 +0530

    String Example

---
+++ src/StringExamples/ConvertStringToIntegerAndWisevarsa.java
@@ -0,0 +1,50 @@
+package StringExamples;
+
+import java.math.BigDecimal;
+
+//Convert String Integer
+//Convert integer to String
+public class ConvertStringToIntegerAndWisevarsa {
+
+	public static void main(String[] args) {
+		
+		String numberString= "50";
+		Integer num= Integer.parseInt(numberString);
+		
+		System.out.println("Convert string to integer: " +num);
+		
+		String string= num.toString();
+		System.out.println("Convert number to string: " +string);
+		
+		// String to Double
+        String doubleStr = "45.67";
+        Double doubleVal = Double.parseDouble(doubleStr);
+        System.out.println("Convert String to Double: " + doubleVal);
+        
+     // Double to String
+        String doubleToStr = doubleVal.toString();
+        System.out.println("Convert Double to String: " + doubleToStr);
+        
+        // String to Long
+        String longStr = "123456789";
+        Long longVal = Long.parseLong(longStr);
+        System.out.println("Convert String to Long: " + longVal);
+
+        // Long to String
+        String longToStr = longVal.toString();
+        System.out.println("Convert Long to String: " + longToStr);
+        
+        // String to Float
+        String floatStr = "12.34";
+        Float floatVal = Float.parseFloat(floatStr);
+        System.out.println("String to Float: " + floatVal);
+        System.out.println("Float to String: " + floatVal.toString());
+		
+     // String to BigDecimal
+        String bigDecimalStr = "9876543210.12345";
+        BigDecimal bigDecimalVal = new BigDecimal(bigDecimalStr);
+        System.out.println("String to BigDecimal: " + bigDecimalVal);
+        System.out.println("BigDecimal to String: " + bigDecimalVal.toString());
+	}
+
+}
